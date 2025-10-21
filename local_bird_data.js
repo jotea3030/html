fe802f0df5299be723e611f6ff2cd7bfda396c2f
John Teagle <tea3gle@gmail.com>
2025-10-21 14:13:35 -0700

    Add local bird data array with audio information
    This file contains a global array of bird data, including names, scientific names, audio URLs, and recordist information.

---
+++ local_bird_data.js
@@ -0,0 +1,16 @@
+// local_bird_data.js — global `allBirds` array pointing to same-origin audio files in ./audio/
+const allBirds = [
+  { name: "American Robin", scientific: "Turdus migratorius", info: "Cheerful caroling song", audioUrl: "audio/XC134898-LS100612_Turdus_migratorius.mp3", recordist: "Paul Driver", xcId: "134898" },
+  { name: "Northern Cardinal", scientific: "Cardinalis cardinalis", info: "Clear whistles", audioUrl: "audio/XC413729-NOCA.mp3", recordist: "Andrew Spencer", xcId: "413729" },
+  { name: "Blue Jay", scientific: "Cyanocitta cristata", info: "Harsh calls", audioUrl: "audio/XC51630-Blue_Jay.mp3", recordist: "Greg Irving", xcId: "51630" },
+  { name: "Black-capped Chickadee", scientific: "Poecile atricapillus", info: "Chick-a-dee-dee-dee", audioUrl: "audio/XC51188-Chickadee__Black-capped.mp3", recordist: "Paul Driver", xcId: "51188" },
+  { name: "Song Sparrow", scientific: "Melospiza melodia", info: "Melodious song", audioUrl: "audio/XC420595-SOSP.mp3", recordist: "Andrew Spencer", xcId: "420595" },
+  { name: "Wood Thrush", scientific: "Hylocichla mustelina", info: "Flutelike phrases", audioUrl: "audio/XC413686-WOTH.mp3", recordist: "Andrew Spencer", xcId: "413686" },
+  { name: "Eastern Meadowlark", scientific: "Sturnella magna", info: "Clear whistles", audioUrl: "audio/XC83562-Meadowlark__Eastern.mp3", recordist: "Paul Driver", xcId: "83562" },
+  { name: "Baltimore Oriole", scientific: "Icterus galbula", info: "Rich warbles", audioUrl: "audio/XC344942-baltimore_oriole_song.mp3", recordist: "Paul Marvin", xcId: "344942" },
+  { name: "House Wren", scientific: "Troglodytes aedon", info: "Bubbling, cascading song", audioUrl: "audio/XC413697-HOWR.mp3", recordist: "Andrew Spencer", xcId: "413697" },
+  { name: "Red-winged Blackbird", scientific: "Agelaius phoeniceus", info: "Conk-la-ree", audioUrl: "audio/XC420591-RWBL.mp3", recordist: "Andrew Spencer", xcId: "420591" },
+  { name: "White-throated Sparrow", scientific: "Zonotrichia albicollis", info: "Whistled phrase", audioUrl: "audio/XC420585-WTSP.mp3", recordist: "Andrew Spencer", xcId: "420585" },
+  { name: "Common Yellowthroat", scientific: "Geothlypis trichas", info: "Witchity-witchity", audioUrl: "audio/XC420588-COYE.mp3", recordist: "Andrew Spencer", xcId: "420588" }
+];
+// global binding expected by index.html
